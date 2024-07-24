52250bf80cdb57409a9a187fe3af9f0bbb014644
Omaar-Mohamed <102541769+Omaar-Mohamed@users.noreply.github.com>
2024-07-24 19:34:32 +0300

    signin with google issue fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:jokes_app/firebase_options.dart';
 import 'package:jokes_app/jokes_app.dart';
 
 import 'core/routing/app_router.dart';
 import 'features/login/cubit/AuthCubit.dart';
+import 'firebase_options.dart';
 
 Future <void>  main() async {
   WidgetsFlutterBinding.ensureInitialized();
